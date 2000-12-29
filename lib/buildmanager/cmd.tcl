1975c12d8abc42b3d2eaa8cfadb607b3cb10da05
mengel <>
2000-12-29 16:37:47 +0000

    cmd button fixes, refactoring

--- lib/buildmanager/cmd.tcl
+++ lib/buildmanager/cmd.tcl
@@ -22,9 +22,33 @@ proc cmd_substitute {string s} {
     return $result
 }
 
-proc cmd_parallel { string {list {xxxxxxxxxx}} } {
+proc cmd_activelist { string } {
+    global sessionlist out_of_the_loop
+    global history
+
+     # update command entry widget, history
+    .cmd.e delete 0 end
+    .cmd.e insert 0 $string
+    .cmd.e selection range 0 end
+    lappend history $string
+    set histslot [llength $history]
+
+    # build session list
+    set list {}
+    foreach s $sessionlist {
+       if { [info exists out_of_the_loop($s)] && $out_of_the_loop($s) } {
+	    # nothing
+       } else {
+	    lappend list $s
+       }
+    }
+    return $list
+}
+
+proc cmd_parallel { string {list None} } {
     global sessionlist os_dat global history histslot
     global out_of_the_loop
+
 
      # some commands *really* should take turns...
 
@@ -35,46 +59,24 @@ proc cmd_parallel { string {list {xxxxxxxxxx}} } {
 	  [string match {*upd*product*} $string]  ||
 	  [string match {*upd*install*} $string]  } {
 
-         return [cmd_taketurns $string $list]
+         return [cmd_taketurns $string]
      }
-    .cmd.e delete 0 end
-    .cmd.e insert 0 $string
-    .cmd.e selection range 0 end
-    lappend history $string
-    set histslot [llength $history]
-    # puts "history is $history"
-    if { "$list" == {xxxxxxxxxx} } {
-	set list $sessionlist
+    if { "$list" == "None" } {
+	set list [cmd_activelist $string]
     }
     foreach s $list {
-        if { ![info exists out_of_the_loop($s)] || !$out_of_the_loop($s)} {
-	    set result [cmd_substitute $string $s]
-	    exp_send -i $s "$result\r"
-	}
-    }
-}
+	set result [cmd_substitute $string $s]
+	exp_send -i $s "$result\r"
+    } 
+} 
 
-proc cmd_taketurns { string {list {xxxxxxxxxx}} } {
+proc cmd_taketurns { string {list None} } {
     global pending1 pending2 sessionlist history histslot
     global out_of_the_loop
 
-    if { "$list" == "$sessionlist" } {
-	.cmd.e delete 0 end
-	.cmd.e insert 0 $string
-	.cmd.e selection range 0 end
-        lappend history $string
-        set histslot [llength $history]
-        # puts "history is $history"
-    }
-    if { "$list" ==  {xxxxxxxxxx}} {
-	set list {}
-        foreach s $sessionlist {
-           if { [info exists out_of_the_loop($s)] && $out_of_the_loop($s) } {
-		# nothing
-           } else {
-		lappend list $s
-           }
-        }
+
+    if { "$list" == "None"} {
+	set list [cmd_activelist $string]
     }
     if { "$list" == "" } {
 	return
@@ -104,15 +106,21 @@ proc cmd_next {} {
 
 proc cmd_return_takesturns {} {
     proc do_cmd_bindings { w } {
-    bind $w.e <Key-Return> {cmd_taketurns [%W get] $sessionlist}
-    bind $w.e <Key-KP_Enter> {%W selection range 0 end; cmd_parallel [%W get]}
+    bind $w.e <Key-Return> {cmd_taketurns [%W get]}
+    bind $w.e <Key-KP_Enter> {cmd_parallel [%W get]}
     }
 }
 
 proc cmd_return_parallel {} {
     proc do_cmd_bindings { w } {
-    bind $w.e <Key-KP_Enter> {cmd_taketurns [%W get] $sessionlist}
-    bind $w.e <Key-Return> {%W selection range 0 end; cmd_parallel [%W get]}
+    bind $w.e <Key-KP_Enter> {cmd_taketurns [%W get]}
+    bind $w.e <Key-Return> {cmd_parallel [%W get]}
     }
 }
+
+
+
+
+
+
 
